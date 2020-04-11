e181bbe5f9a70fc4352fa24cc16cde2708bb5aee
Roger Mitchell <rogeramitchell@gmail.com>
2020-04-11 10:56:05 -0400

    let's load up styles and scripts

--- force-app/main/default/lwc/svelte/svelte.js
+++ force-app/main/default/lwc/svelte/svelte.js
@@ -1,3 +1,14 @@
 import { LightningElement } from 'lwc';
+import { loadScript, loadStyle } from 'lightning/platformResourceLoader';
 
-export default class Svelte extends LightningElement {}
+// import Svelte static resource
+import svelteApp from '@salesforce/resourceUrl/Svelte';
+
+export default class Svelte extends LightningElement {
+  connectedCallback() {
+    Promise.all([
+      loadScript(this, svelteApp + '/bundle.js'),
+      loadStyle(this, svelteApp + '/bundle.css'),
+    ])
+  }
+}
